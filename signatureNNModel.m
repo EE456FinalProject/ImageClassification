e4ab90e5dfb2f19de85adeb18069c3559e2abdd8
Park <jjp6315@psu.edu>
2023-12-05 21:55:32 -0500

    Done data processing

--- signatureNNModel.m
+++ signatureNNModel.m
@@ -7,22 +7,64 @@ file_paths = {'processed_matrices\signatures\Brian\signatureData.mat',
     'processed_matrices\signatures\John\signatureData.mat', 
     'processed_matrices\signatures\JohnKorean\signatureData.mat'};
 
-% Initialize the AllData matrix
-AllData = zeros(60, 27648);
+% Initialize the matrices for training, validation, and testing
+TrainData = zeros(48, 27648);  
+TrainLabel = zeros(48, 1);
+ValidData = zeros(6, 27648);  
+ValidLabel = zeros(6, 1);
+TestData = zeros(6, 27648); 
+TestLabel = zeros(6, 1);
 
 % Loop through each MAT file
 for file_index = 1:length(file_paths)
     % Load the MAT file
     loaded_data = load(file_paths{file_index});
 
-    % Assuming the variable name is 'signatureData'
     % Adjust this according to your actual variable name
     signatureData = loaded_data.signatureData;
 
-    % Place the data into AllData
-    start_row = (file_index - 1) * 20 + 1;
-    end_row = file_index * 20;
-    AllData(start_row:end_row, :) = signatureData;
+    start_row_train = (file_index - 1) * 16 + 1;
+    end_row_train = file_index * 16;
+
+    start_row_valid = (file_index - 1) * 2 + 1;
+    end_row_valid = file_index * 2;
+
+    start_row_test = (file_index - 1) * 2 + 1;
+    end_row_test = file_index * 2;
+
+    % Training set
+    TrainData(start_row_train:end_row_train, :) = signatureData(1:16, :);
+    TrainLabel(start_row_train:end_row_train, :) = file_index;
+
+    % Validation set
+    ValidData(start_row_valid:end_row_valid, :) = signatureData(17:18, :);
+    ValidLabel(start_row_valid:end_row_valid, :) = file_index;
+
+    % Testing set
+    TestData(start_row_test:end_row_test, :) = signatureData(19:20, :);
+    TestLabel(start_row_test:end_row_test, :) = file_index;
 end
 
-disp(size(AllData));
+% Shuffle the training data
+num_samples_train = size(TrainData, 1);
+shuffled_indices_train = randperm(num_samples_train);
+
+TrainData = TrainData(shuffled_indices_train, :);
+TrainLabel = TrainLabel(shuffled_indices_train, :);
+
+% Shuffle the validation data
+num_samples_valid = size(ValidData, 1);
+shuffled_indices_valid = randperm(num_samples_valid);
+
+ValidData = ValidData(shuffled_indices_valid, :);
+ValidLabel = ValidLabel(shuffled_indices_valid, :);
+
+% Shuffle the testing data
+num_samples_test = size(TestData, 1);
+shuffled_indices_test = randperm(num_samples_test);
+
+TestData = TestData(shuffled_indices_test, :);
+TestLabel = TestLabel(shuffled_indices_test, :);
+
+%____________________________________________________________
+% Start Training network
